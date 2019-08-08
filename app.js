1298742a4d56afebee44075238eb2c64b091deb5
Vishal Kuvalekar <vishalkuvalekar@gmail.com>
2019-08-08 19:28:10 +0530

    setting response

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const  http = require('http');
+const http = require('http');
 
 function reqListner(req, res) {
     console.log(req);
+    // it will stop server
+    // process.exit();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+        <html>
+            <head><title>Test</title></head>
+            <body>Hello from node.js server</body>
+        </html>
+    `);
+    res.end();
 }
 
 const server = http.createServer(reqListner);
